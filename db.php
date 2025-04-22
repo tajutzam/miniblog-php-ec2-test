7f3a6d63120afdcda988cac3e692bf46a48e2e95
tajutzam <mohammadtajutzamzami07@gmail.com>
2025-04-22 20:18:12 +0700

    db

--- db.php
+++ db.php
@@ -1,4 +1,15 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=news_db", "root", "root");
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
+$host = "todolistdb.cbyieygqmvuh.ap-southeast-2.rds.amazonaws.com";  // Ganti dengan endpoint RDS
+$dbname = "todolist_db";  // Nama database yang digunakan
+$username = "root";  // Username RDS kamu
+$password = "Rahasia123.";
+
+try {
+    // Menghubungkan ke RDS MySQL menggunakan PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Set error mode untuk menampilkan error jika ada masalah
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connection successful!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
